510085beb94dc82c2e946e49b3a153659065772f
Dane M Goodman <DaneGoodman85@gmail.com>
2020-06-20 14:42:24 -0700

    Alright, this is a good first start

--- garden-finder/src/components/App/App.js
+++ garden-finder/src/components/App/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Home from "../Home/Home.js";
 
 function App() {
   return (
     <div>
-      <h1>HELLO WORLD</h1>
+      <Home />
     </div>
   );
 }
---
+++ garden-finder/src/components/Home/Home.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class Home extends Component {
+    state = {
+        title: " T H E   G A R D E N S "
+    }
+
+    render() {
+        return (
+            <div className="landing-box container-fluid">
+                <div id="landing-div" className="jumbotron bg-transparent rounded-0">
+                    {/* <h1 id="landing-title">T H E    G A R D E N S</h1> */}
+                    <h1 id="landing-title">{this.state.title}</h1>
+                    <div className="text-center">
+                        {/* <a href="/login" className="top-button btn-lg m-1 btn btn-success" onClick={()=> this.handleClick()}>Login</a> */}
+                        <button className="top-button btn-lg m-1 btn btn-success" onClick={() => this.handleClick()}>Login</button>
+                        {/* <button onClick={()=> this.handleClick()}>Console Log </button> */}
+                        <a href="/express_backend" className="top-button btn-lg m-1 btn btn-success">Register</a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Home; 

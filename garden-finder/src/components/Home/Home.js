6b74ec8bff91457d5990b252e26d63ff2f3ad3d1
Dane M Goodman <DaneGoodman85@gmail.com>
2020-06-20 17:59:45 -0700

    Landing page constructed without CSS

--- garden-finder/src/components/Home/Home.js
+++ garden-finder/src/components/Home/Home.js
@@ -6,22 +6,33 @@ class Home extends Component {
         title: " T H E   G A R D E N S "
     }
 
-    render() {
-        return (
-            <div className="landing-box container-fluid">
-                <div id="landing-div" className="jumbotron bg-transparent rounded-0">
-                    {/* <h1 id="landing-title">T H E    G A R D E N S</h1> */}
-                    <h1 id="landing-title">{this.state.title}</h1>
-                    <div className="text-center">
-                        {/* <a href="/login" className="top-button btn-lg m-1 btn btn-success" onClick={()=> this.handleClick()}>Login</a> */}
-                        <button className="top-button btn-lg m-1 btn btn-success" onClick={() => this.handleClick()}>Login</button>
-                        {/* <button onClick={()=> this.handleClick()}>Console Log </button> */}
-                        <a href="/express_backend" className="top-button btn-lg m-1 btn btn-success">Register</a>
-                    </div>
-                </div>
-            </div>
-        )
-    }
-}
+       render() {
+           return (
+               <div className="landing-box container-fluid">
+                   <div id="landing-div" className="jumbotrong bg-transparent rounded-0">
+                       <h1 id="landing-title"> { this.state.title } </h1>
+                       <div className="text-center">
+                       </div>
+                   </div>
+               </div>
+           )
+       }
+//     render() {
+//         return (
+//             <div className="landing-box container-fluid">
+//                 <div id="landing-div" className="jumbotron bg-transparent rounded-0">
+//                     {/* <h1 id="landing-title">T H E    G A R D E N S</h1> */}
+//                     <h1 id="landing-title">{this.state.title}</h1>
+//                     <div className="text-center">
+//                         {/* <a href="/login" className="top-button btn-lg m-1 btn btn-success" onClick={()=> this.handleClick()}>Login</a> */}
+//                         <button className="top-button btn-lg m-1 btn btn-success" onClick={() => this.handleClick()}>Login</button>
+//                         {/* <button onClick={()=> this.handleClick()}>Console Log </button> */}
+//                         <a href="/express_backend" className="top-button btn-lg m-1 btn btn-success">Register</a>
+//                     </div>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
 
 export default Home; 

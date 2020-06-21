a141bd0bbd033c135ab8431787bc3988e072d06f
Dane M Goodman <DaneGoodman85@gmail.com>
2020-06-21 14:35:57 -0700

    Set up the react-router, now gonna take it for a test run

--- garden-finder/src/components/App/App.js
+++ garden-finder/src/components/App/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from "../Home/Home.js";
+import Map from '../elements/Map/Map.js';
 
 function App() {
   return (
     <div>
-      <Home />
+      <BrowserRouter>
+        <React.Fragment>
+          <Switch>
+            <Route path='/' component={ Home } exact />
+            <Route path='/map' component={ Map } exact />
+          </Switch>
+        </React.Fragment>
+      </BrowserRouter>
     </div>
   );
 }

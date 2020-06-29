d8c892c365080c678eaac2ba9bde28c83f8f0b64
Dane M Goodman <DaneGoodman85@gmail.com>
2020-06-29 15:58:14 -0700

    Putting in a header

---
+++ garden-finder/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css'; 
+
+const Header = () => {
+    return (
+        <div className="tmnt-header">
+            <div className="tmnt-header-content">
+                T H E   G A R D E N S
+            </div>
+        </div>
+    )
+}
+
+export default Header;

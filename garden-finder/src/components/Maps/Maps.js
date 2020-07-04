5771bfadc9682b6fcd3d4e579d81d2590df91e8a
Dane M Goodman <DaneGoodman85@gmail.com>
2020-07-04 11:39:39 -0700

    API is up and running, now I just need to work on parsing the data

--- garden-finder/src/components/Maps/Maps.js
+++ garden-finder/src/components/Maps/Maps.js
@@ -6,17 +6,49 @@ import { BASE_URL, VERSION, EXT, API_KEY } from '../../config.js';
 class Maps extends Component {
     state = {
         title: " YOU LIKE MAPS? WE GOT MAPS!",
-        data: null,
+        mapData: [],
     };
 
-    componentDidMount() {
+
+    // renderMapData = () => {
+    //     const mapElements = this.state.mapData.map( (element, i) => {
+    //         return (
+    //             <div key={i} className='map-elements'>
+    //                 {element}
+    //             </div>
+    //         )
+    //     })
+    //         return mapElements;
+    // }
+
+    handleClick = () => {
         fetch(`https://${BASE_URL}/search/${VERSION}/search/91942.${EXT}?key=${API_KEY}`)
-          .then(response => response.json())
-          .then(data => {
-              console.log(data)
-           this.setState({ data });
-      }
-          )};
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                this.setState({ mapData: data });
+            }
+            )
+    };
+
+    mapLog = () => {
+        let data = this.state.mapData; 
+        console.log(data.results[3]);
+    }
+
+
+    // componentDidMount() {
+    //     fetch(`https://${BASE_URL}/search/${VERSION}/search/91942.${EXT}?key=${API_KEY}`)
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             console.log(data)
+    //             this.setState({ mapData: data });
+    //         }
+    //         )
+    // };
+
+
+
 
     render() {
         return (
@@ -24,17 +56,21 @@ class Maps extends Component {
                 <Header />
 
                 <div className="flex-container">
-                {this.state.title}
-                <div className="map">
-                    I will be a map
+                    {this.state.title}
+                    <div className="map">
+                        I will be a map
                 </div>
-                <form>
-                    <label>
-                        Zipcode:
+                    <form>
+                        <label>
+                            Zipcode:
                     <input type="text" name="Zipcode" />
-                    </label>
-                </form>
-                <button className="top-button btn-lg m-1 btn btn-success">Search</button>
+                        </label>
+                    </form>
+                    <button className="top-button btn-lg m-1 btn btn-success" onClick={() => this.handleClick()}>Search</button>
+                    <button className="top-button btn-lg m-1 btn btn-success" onClick={() => this.mapLog()}>Console Log</button>
+                </div>
+                <div className='results'>
+                    {/* {this.renderMapData()} */}
                 </div>
             </div>
 

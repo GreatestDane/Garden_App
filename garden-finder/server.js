60b8945a5f82c22e73ea9b254ad8134184056349
Dane M Goodman <DaneGoodman85@gmail.com>
2020-07-06 21:52:30 -0700

    Express server running on port 3001, and I set up 3001 as the proxy in the package.json

--- garden-finder/server.js
+++ garden-finder/server.js
@@ -4,11 +4,14 @@ const cors = require('cors');
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors);
 app.use(bodyParser.json());
+
+app.use(express.static(__dirname + "/public"))
 
 app.get('/', (req, res) => {
     res.send('HELLO WORLD');
